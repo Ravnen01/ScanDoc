e0ac7fc48047312bb3486ece9d45a710c38b99f6
Lecomte Corentin <lecomte.corentin@gmail.com>
2019-10-21 14:46:39 +0200

    Add lib tesseract

--- app/src/main/java/com/lecor/scandoc/MainActivity.kt
+++ app/src/main/java/com/lecor/scandoc/MainActivity.kt
@@ -1,12 +1,84 @@
 package com.lecor.scandoc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.googlecode.tesseract.android.TessBaseAPI
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE=123
+    val MY_PERMISSIONS_REQUEST_CAMERA=456
+    val tess=TessBaseAPI()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_CAMERA)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+        bTakePhoto.setOnClickListener {
+            initTesseract()
+            dispatchTakePictureIntent()
+        }
     }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when(requestCode){
+            MY_PERMISSIONS_REQUEST_CAMERA ->{
+                val imageBitmap = data?.extras?.get("data") as Bitmap
+
+            }
+        }
+    }
+
+    fun initTesseract(){
+        var datapath = baseContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)?.absolutePath + "/tesseract/"
+        val language = "eng"
+        val dir = File(datapath + "tessdata/")
+        if (!dir.exists())
+            dir.mkdirs()
+        tess.init(datapath, language)
+    }
+
 }
